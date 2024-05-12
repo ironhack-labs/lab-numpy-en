7d1475272609fda4cef10f5e23a2860267af4d81
“Lylrg” <lydia.aa@hotmail.es>
2024-05-12 23:23:06 +0200

    LydiaNumpyLab

--- your-code/main.py
+++ your-code/main.py
@@ -2,70 +2,78 @@
 
 #[your code here]
 
+import numpy as np
 
 #2. Print the NUMPY version and the configuration.
 
 #[your code here]
-
+print(np.__version__)
+print(np.show_config())
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
 #[your code here]
+a = np.random.random((2,3,5))
+#np.random.randint(100, size =(2,3,5))
+#np.random.rand(2,3,5) specified the shape of the array
+## create  alist and transform to a array
 
 #4. Print a.
+print(a)
 
 #[your code here]
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
+b = np.ones((5, 2, 3))
 
-#[your code here]
 
 #6. Print b.
-
+print(b)
 #[your code here]
 
 #7. Do a and b have the same size? How do you prove that in Python code?
+proof = b.size == a.size
+print(f"Are a and b the same size: {proof}")
 
-#[your code here]
+# size is the amount of values
 
 #8. Are you able to add a and b? Why or why not?
 
-#[your code here]
 
+# we cannot add because we have different shapes
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
-
-#[your code here]
+#c = b.T  # now 3x2x5 b is 0,1,2
+c = np.transpose(b, axes=[1,2,0])
+print(c)
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 
-#[your code here]
-
+d = c + a
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
-
-#[your code here]
-
+print(a)
+print(d)
+# d is c plus 1 in each value
 
 #12. Multiply a and c. Assign the result to e.
-
-#[your code here]
+e = a*c
 
 
 #13. Does e equal to a? Why or why not?
 
-#[your code here]
-
-
-
+proof2 = e == a
+print(f"Are e and a equal: {proof2}")
+# e equals a because we * a by c and c is equal to one
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
-#[your code here]
-
+d_max = np.max(d)
+d_min = np.min(d)
+d_mean =np.mean(d)
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
-#[your code here]
+f = np.empty((2,3,5))
 
 
 """
@@ -78,7 +86,30 @@ In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
 
-#[your code here]
+for i, value_i in enumerate(d):
+    for j, value_j in enumerate(value_i):
+        for index, value in enumerate(value_j):
+            if value > d_min and value < d_mean:
+                value = 25
+                f[i][j][index] = value
+        
+            if value > d_mean and value < d_max:
+                value = 75
+                f[i][j][index] = value
+        
+            if value == d_mean:
+                value = 50
+                f[i][j][index] = value
+        
+            if value == d_min:
+                value = 0
+                f[i][j][index] = value
+        
+            if value == d_max:
+                value = 100
+                f[i][j][index] = value
+
+    
 
 
 
@@ -104,8 +135,8 @@ array([[[ 75.,  75.,  75.,  25.,  75.],
         [ 25.,  75.,   0.,  75.,  75.]]])
 """
 
-#[your code here]
-
+print(d)
+print(f)
 
 
 """
@@ -120,5 +151,4 @@ array([[[ 'D',  'D',  'D',  'B',  'D'],
         [ 'B',  'D',   'A',  'D', 'D']]])
 Again, you don't need Numpy in this question.
 """
-
-#[your code here]
+f = np.array(f, dtype=np.dtype(str))
