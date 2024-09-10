14ae4796a4024a4fef54fc8a66e07f45c35e0c73
pbeles <pbeles@gmail.com>
2024-09-10 16:54:12 +0200

    final 2nd week 2nd lab

--- your-code/main.py
+++ your-code/main.py
@@ -1,72 +1,80 @@
 #1. Import the NUMPY package under the name np.
+import numpy as np
 
 #[your code here]
 
 
 #2. Print the NUMPY version and the configuration.
 
-#[your code here]
+print(np.__version__)
+print(np.show_config())
 
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
-#[your code here]
+a = np.random.random((2, 3, 5))
 
 #4. Print a.
 
-#[your code here]
+print("Array a:\n", a)
+
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
 
-#[your code here]
+b = np.ones((5, 2, 3))
 
 #6. Print b.
 
-#[your code here]
+print("Array b:\n", b)
 
 #7. Do a and b have the same size? How do you prove that in Python code?
 
-#[your code here]
+print("a and b have the same size:", a.size == b.size)
 
 #8. Are you able to add a and b? Why or why not?
 
-#[your code here]
-
+try:
+    c = a + b
+except ValueError as e:
+    print(f"Error: {e}")
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
 
-#[your code here]
+c = b.transpose((1, 2, 0))
+print("Transposed array c:\n", c)
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 
-#[your code here]
+d = a + c
+print("Array d (a + c):\n", d)
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 
-#[your code here]
-
+print("Array a:\n", a)
+print("Array d:\n", d)
 
 #12. Multiply a and c. Assign the result to e.
 
-#[your code here]
-
+e = a * c
+print("Array e (a * c):\n", e)
 
 #13. Does e equal to a? Why or why not?
 
-#[your code here]
-
-
+print("Is e equal to a?:", np.array_equal(e, a))
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
-#[your code here]
+d_max = d.max()
+d_min = d.min()
+d_mean = d.mean()
 
+print(f"d_max: {d_max}, d_min: {d_min}, d_mean: {d_mean}")
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
-#[your code here]
-
+f = np.empty_like(d)
+print("Empty array f:\n", f)
 
 """
 #16. Populate the values in f. For each value in d, if it's larger than d_min but smaller than d_mean, assign 25 to the corresponding value in f.
@@ -78,10 +86,21 @@ In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
 
-#[your code here]
+for i in range(d.shape[0]):
+    for j in range(d.shape[1]):
+        for k in range(d.shape[2]):
+            if d[i, j, k] > d_min and d[i, j, k] < d_mean:
+                f[i, j, k] = 25
+            elif d[i, j, k] == d_mean:
+                f[i, j, k] = 50
+            elif d[i, j, k] > d_mean and d[i, j, k] < d_max:
+                f[i, j, k] = 75
+            elif d[i, j, k] == d_max:
+                f[i, j, k] = 100
+            else:
+                f[i, j, k] = 0
 
-
-
+print("Array f after labeling:\n", f)
 
 """
 #17. Print d and f. Do you have your expected f?
@@ -104,8 +123,8 @@ array([[[ 75.,  75.,  75.,  25.,  75.],
         [ 25.,  75.,   0.,  75.,  75.]]])
 """
 
-#[your code here]
-
+print("Array d:\n", d)
+print("Array f (expected values):\n", f)
 
 
 """
@@ -121,4 +140,20 @@ array([[[ 'D',  'D',  'D',  'B',  'D'],
 Again, you don't need Numpy in this question.
 """
 
-#[your code here]
+f_string = np.empty_like(f, dtype=str) #i have no idea if this is correct
+
+for i in range(f.shape[0]):
+    for j in range(f.shape[1]):
+        for k in range(f.shape[2]):
+            if f[i, j, k] == 0:
+                f_string[i, j, k] = 'A'
+            elif f[i, j, k] == 25:
+                f_string[i, j, k] = 'B'
+            elif f[i, j, k] == 50:
+                f_string[i, j, k] = 'C'
+            elif f[i, j, k] == 75:
+                f_string[i, j, k] = 'D'
+            elif f[i, j, k] == 100:
+                f_string[i, j, k] = 'E'
+
+print("Array f with string labels:\n", f_string)
