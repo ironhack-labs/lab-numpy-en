66c8ca1c81b74e4f2b2244db71fe5faea6e9de21
Marina Castillo <marina.castillo.ariza@gmail.com>
2024-09-12 23:47:46 +0200

    without bonus

--- your-code/main.py
+++ your-code/main.py
@@ -1,71 +1,79 @@
 #1. Import the NUMPY package under the name np.
 
-#[your code here]
-
+import numpy as np
 
 #2. Print the NUMPY version and the configuration.
 
-#[your code here]
-
+print(np.__version__)
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
-
-#[your code here]
-
+a = np.random.randn(2,3,5)
 #4. Print a.
+print(a)
 
-#[your code here]
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
 
-#[your code here]
+b = np.ones((5,2,3))
 
 #6. Print b.
 
-#[your code here]
-
+print(b)
 #7. Do a and b have the same size? How do you prove that in Python code?
 
-#[your code here]
-
+print(a.size)
+print(b.size)
+if a.size == b.size:
+    print(True)
+else:
+    print(False)
 #8. Are you able to add a and b? Why or why not?
 
-#[your code here]
-
+# a + b
+# a and b have different shapes and cannot be added (operands could not be broadcast together with shapes (2,3,5) (5,2,3) )
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
 
-#[your code here]
+c = b.reshape(2,3,5)
+#print(c)
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 
-#[your code here]
-
+d = a + c
+#print(d)
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 
-#[your code here]
-
-
+print(a)
+print(d)
+#The values in D are the sum of each element of both arrays A a C: I added 1 to each element of a.
 #12. Multiply a and c. Assign the result to e.
 
-#[your code here]
-
+e = a * d
+print(e)
 
 #13. Does e equal to a? Why or why not?
 
-#[your code here]
+comparison = (a == e)
+are_equal = np.all(comparison)
+print(are_equal)
+# They are not equal because the values of e are the result of multiplying ieach element of a by every elemento from d.
 
 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
-#[your code here]
+d_max = d.max
+d_min = d.min
+d_mean = d.mean
+print(d_max)
+print(d_min)
+print(d_mean)
 
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
-#[your code here]
+f = np.empty((2, 3, 5))
 
 
 """
@@ -78,13 +86,19 @@ In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
 
-#[your code here]
-
+f[(d > d_min) & (d < d_mean)] = 25
+f[(d > d_mean) & (d < d_max)] = 75
+f[d == d_mean] = 50
+f[d == d_min] = 0
+f[d == d_max] = 100
 
 
 
 """
 #17. Print d and f. Do you have your expected f?
+
+print(f)
+print(d)
 For instance, if your d is:
 array([[[1.85836099, 1.67064465, 1.62576044, 1.40243961, 1.88454931],
         [1.75354326, 1.69403643, 1.36729252, 1.61415071, 1.12104981],
@@ -104,8 +118,8 @@ array([[[ 75.,  75.,  75.,  25.,  75.],
         [ 25.,  75.,   0.,  75.,  75.]]])
 """
 
-#[your code here]
-
+print(f)
+print(d)
 
 
 """
