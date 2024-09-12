323244f4151c0fdaf4eeb2314e6dea90b66fb20f
tejal-karthik <karthi.karupp@gmail.com>
2024-09-12 22:00:31 +0200

    Lab_numpy_karthik

--- your-code/main.py
+++ your-code/main.py
@@ -1,72 +1,100 @@
 #1. Import the NUMPY package under the name np.
 
 #[your code here]
-
+import numpy as np
 
 #2. Print the NUMPY version and the configuration.
 
 #[your code here]
-
+np.show_config()
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
 #[your code here]
+# random_numbers = np.random.random(10)
+# random_numbers1 = np.random.randint(1,100)
+a = np.random.random((2, 3, 5)) # (block, rows, columns)
 
 #4. Print a.
 
 #[your code here]
+print(a)
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
 
 #[your code here]
+b = np.ones((5,2,3))
 
 #6. Print b.
 
 #[your code here]
+print(b)
 
 #7. Do a and b have the same size? How do you prove that in Python code?
 
 #[your code here]
+if a.size == b.size:
+    print("yes, they have the same size")
+else:
+    print("No, they don't have the same size")
 
 #8. Are you able to add a and b? Why or why not?
 
 #[your code here]
 
+if a.shape == b.shape:  
+     print ("Yes, shapes of a & b are identical and can be added: ")
+else:    
+     print("No, shapes of a & b are different and so they cannot be added:")
+
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
 
 #[your code here]
+c = np.transpose(b, (1,2,0)) # Transpose b so it has the same shape as a - just reording the axis
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 
 #[your code here]
+d = a + c       # because of similar shape of a & c, they can be added
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 
 #[your code here]
-
+print(a)
+print("value of a above and value of d below\n")
+print(d)
 
 #12. Multiply a and c. Assign the result to e.
 
 #[your code here]
-
+e = np.multiply(a,c)
+print("product of a&c below\n")
+print(e)
 
 #13. Does e equal to a? Why or why not?
 
 #[your code here]
-
+equal_result = (e == a)
+print("Equality Comparison Result:")
+print(equal_result)
 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
 #[your code here]
-
+d_max = np.max(d)
+d_min = np.min(d)
+d_mean = np.mean(d)
+print("Max Value of d:", d_max)
+print("Min Value of d:", d_min)
+print("Mean Value of d:", d_mean)
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
 #[your code here]
-
+f = np.empty(d.shape)
 
 """
 #16. Populate the values in f. For each value in d, if it's larger than d_min but smaller than d_mean, assign 25 to the corresponding value in f.
@@ -79,6 +107,13 @@ Note: you don't have to use Numpy in this question.
 """
 
 #[your code here]
+
+# Populate the values in `f` based on the conditions
+f = np.where(d == d_min, 0, f)  # Assign 0 where d equals d_min
+f = np.where(d == d_max, 100, f)  # Assign 100 where d equals d_max
+f = np.where((d > d_min) & (d < d_mean), 25, f)  # Assign 25 where d is between d_min and d_mean
+f = np.where((d > d_mean) & (d < d_max), 75, f)  # Assign 75 where d is between d_mean and d_max
+f = np.where(d == d_mean, 50, f)  # Assign 50 where d equals d_mean
 
 
 
@@ -105,7 +140,9 @@ array([[[ 75.,  75.,  75.,  25.,  75.],
 """
 
 #[your code here]
-
+print(d)
+print("printing f below: ")
+print(f)
 
 
 """
@@ -122,3 +159,12 @@ Again, you don't need Numpy in this question.
 """
 
 #[your code here]
+
+# Populate the values in `f` based on the conditions
+f = np.where(d == d_min, 'A', f)  # Assign A where d equals d_min
+f = np.where(d == d_max, 'E', f)  # Assign E where d equals d_max
+f = np.where((d > d_min) & (d < d_mean), 'B', f)  # Assign B where d is between d_min and d_mean
+f = np.where((d > d_mean) & (d < d_max), 'D', f)  # Assign D where d is between d_mean and d_max
+f = np.where(d == d_mean, 'C', f)  # Assign C where d equals d_mean
+
+print(f)
